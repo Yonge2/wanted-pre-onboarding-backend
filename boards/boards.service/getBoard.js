f5a8be1aa72f48603c7643b100b22f2172e7adb6
Yonge2 <dlwodyd25@naver.com>
2023-10-09 00:31:39 +0900

    Feat: 검색 기능 구현

--- boards/boards.service/getBoard.js
+++ boards/boards.service/getBoard.js
@@ -1,21 +1,76 @@
-const Board = require('../../sequelize/models/board');
+const { Op } = require('sequelize')
+const { db } = require('../../sequelize/models/index')
 
-//todo
-// + 키워드 
-//1. default - 공고 등록 시간순
-//2. 거리순
-//3. 해당 스킬순
-module.exports = getBoard = async(req, res)=>{
-    if(req.query.search){
-        //search KeyWord
+
+module.exports = getBoard = async (req, res) => {
+
+    const search = req.query.search
+
+    const condition = (search) ? searchLike(search) : {}
+
+    try {
+
+        const searchResult = await db.Board.findAll({
+            attributes:
+                ['board_id', 'company_id', 'emp_title', 'emp_position', 'emp_skill', 'emp_prize', 'company_region'],
+            where: condition,
+            raw: true,
+        })
+
+        res.status(200).json({ success: true, message: searchResult })
     }
-    try{
-        const a = await Board.findAll({raw: true});
-        console.log(a);
-        res.status(200).json(a);
+    catch (e) {
+
+        console.log("getBoard Err : ", e)
+        res.status(400).json({ success: false, mesaage: "잘못된 요청" })
     }
-    catch(e){
-        console.log(e);
-        res.status(400).json({success: false, mesaage: "잘못된 요청"});
+}
+
+
+/**
+ * 문자로 검색 : 그 문자를 포함한 쿼리결과 반환,
+ * 숫자로 검색 : 검색한 숫자 이상의 채용보상금을 가진 쿼리결과 반환
+ * @param {string} query.search 
+ *
+ */
+const searchLike = (search) => {
+
+    const isNumber = parseInt(search)
+
+    if (!isNaN(isNumber)) {
+        return {
+            emp_prize: {
+                [Op.gte]: isNumber
+            }
+        }
+    }
+    return {
+        [Op.or]: [
+            {
+                emp_title: {
+                    [Op.like]: `%${search}%`
+                }
+            },
+            {
+                emp_position: {
+                    [Op.like]: `%${search}%`
+                }
+            },
+            {
+                emp_skill: {
+                    [Op.like]: `%${search}%`
+                }
+            },
+            {
+                company_id: {
+                    [Op.like]: `%${search}%`
+                }
+            },
+            {
+                company_region: {
+                    [Op.like]: `%${search}%`
+                }
+            }
+        ]
     }
 }
