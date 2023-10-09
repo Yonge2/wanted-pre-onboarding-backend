d2abe813e2dfa86dcec5a0799e07643f7e309d1e
Yonge2 <dlwodyd25@naver.com>
2023-10-09 17:47:13 +0900

    Style: 설명 추가

--- boards/boards.middleware/checkAccess.js
+++ boards/boards.middleware/checkAccess.js
@@ -1,28 +1,33 @@
-//로그인 인증 방법 대신, 기업 회원과 개인 회원 구분을 위한 임의 인증 미들웨어
-//간단하게 body나 header에 id 값을 넣어 판단한다.
-module.exports = checkAccess = (req, res, next)=>{
-    
-    //user code = 0, employer code = 1
+/**
+ * 로그인 인증 방법 대신, 간단하게 header에 id 값을 넣어 기업 회원과 개인 회원 구분을 위한 임의 인증 미들웨어
+ * @param {*} req req.headers.user_id || req.headers.company_id
+ * @param {*} res 잘못된 값 받았을 때의 응답 객체
+ * @param {*} next 기업/개인 판단 후 callback
+ */
+const checkAccess = (req, res, next) => {
+
     const user = req.headers.user_id
-    const employer = req.headers.company_id
+    const company = req.headers.company_id
 
     //개인 회원
-    if(user&&!employer) {
+    if (user && !company) {
         req.id = {
-            code : 'user',
+            code: 'user',
             user_id: user
         }
         next();
     }
     //기업 회원
-    else if(employer&&!user){
+    else if (company && !user) {
         req.id = {
-            code : 'employer',
-            company_id: employer
+            code: 'company',
+            company_id: company
         }
         next();
     }
-    else{
-        return res.status(401).json({success: false, message: "잘못된 인증 정보, headers나 body를 확인하세요."});
+    else {
+        return res.status(401).json({ success: false, message: "잘못된 인증 정보, headers를 확인하세요." });
     }
 }
+
+module.exports = {checkAccess}
