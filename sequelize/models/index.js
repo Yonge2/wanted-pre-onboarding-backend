86cd06b79e3e5ec2993856b0b6ae5fee28ee0a18
Yonge2 <dlwodyd25@naver.com>
2023-10-09 17:52:23 +0900

    Refactor: 주석제거, 모델 간 관계 정의

--- sequelize/models/index.js
+++ sequelize/models/index.js
@@ -1,61 +1,42 @@
 const { Sequelize } = require('sequelize');
-const process = require('process');
-// // const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 
+const env = 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 
 const User = require('./user');
 const UserApply = require('./user_apply');
 const Company = require('./company');
 const Board = require('./board');
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = 
-// }
 
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (
-//       file.indexOf('.') !== 0 &&
-//       file !== basename &&
-//       file.slice(-3) === '.js' &&
-//       file.indexOf('.test.js') === -1
-//     );
-//   })
-//   .forEach(file => {
-//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-//     db[model.name] = model;
-//   });
+const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-const db = {};
-db.sequelize = sequelize;
+const db = {}
+db.sequelize = sequelize
 
-db.User = User;
-db.User_apply = UserApply;
-db.Company = Company;
-db.Board = Board;
+db.User = User
+db.UserApply = UserApply
+db.Company = Company
+db.Board = Board
 
-User.init(sequelize);
-UserApply.init(sequelize);
-Company.init(sequelize);
-Board.init(sequelize);
+db.User.init(sequelize)
+db.UserApply.init(sequelize)
+db.Company.init(sequelize)
+db.Board.init(sequelize)
 
-Company.hasMany(db.Board, {foreignKey: "company_id"})
-Board.belongsTo(db.Company, {foreignKey: "company_id"})
+db.Company.hasMany(db.Board, {foreignKey: "company_id"})
+db.Board.belongsTo(db.Company, {foreignKey: "company_id"})
+
+db.User.hasMany(db.UserApply, {foreignKey: "user_id"})
+db.UserApply.belongsTo(db.User, {foreignKey: "user_id"})
+
+db.Board.hasOne(db.UserApply, {foreignKey: "board_id"})
+db.UserApply.belongsTo(db.Board, {foreignKey: "board_id"})
+
 
 setTimeout(()=>{
-    User.initData();
-    Company.initData();
-}, 1000);
+    User.initData()
+    Company.initData()
+}, 1000)
 
-module.exports = { db };
+module.exports = { db }
